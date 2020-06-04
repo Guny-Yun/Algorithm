c8b669a7b3312553c3d139a5cf4661565dab2df3
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-06-04 17:04:56 +0900

    프로그래머스 lv1 같은숫자는싫어

---
+++ Programmers/Level1/Solution_같은숫자는싫어.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+public class Solution_같은숫자는싫어 {
+	static int[] arr = {1,1,3,3,0,1};
+	public static void main(String[] args) {
+		int[] answer = {};
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(arr[0]);
+		for(int i = 1 ; i < arr.length;i++) {
+			if(arr[i] != list.get(list.size()-1)) {
+				list.add(arr[i]);
+			}
+			else continue;
+		}
+		answer = new int[list.size()];
+		for(int i = 0 ; i < list.size();i++) {
+			answer[i] = list.get(i);
+		}
+		for(int a: answer) {
+			System.out.print(a + " ");
+		}
+	}
+}
