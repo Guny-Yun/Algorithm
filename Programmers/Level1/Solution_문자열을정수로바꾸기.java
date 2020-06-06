a8b4f3b746918facc876974178c51583fa42c9d8
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-06-06 21:56:34 +0900

    프로그래머스 lv.1 문자열을정수로바꾸기

---
+++ Programmers/Level1/Solution_문자열을정수로바꾸기.java
@@ -0,0 +1,13 @@
+package Level1;
+
+public class Solution_문자열을정수로바꾸기 {
+	static String s = "-1234";
+	public static void main(String[] args) {
+		int answer = 0;
+		
+		answer = Integer.parseInt(s);
+		
+		System.out.println(answer);
+	}
+
+}
