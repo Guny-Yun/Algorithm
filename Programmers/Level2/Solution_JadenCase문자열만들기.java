b65b29399476dd577cfb81a300bd01b0f8f367a0
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-07-27 19:42:19 +0900

    프로그래머스 lv.2 JadenCase 문자열 만들기 정답 수정

--- Programmers/Level2/Solution_JadenCase문자열만들기.java
+++ Programmers/Level2/Solution_JadenCase문자열만들기.java
@@ -1,21 +1,30 @@
 package Level2;
 
 public class Solution_JadenCase문자열만들기 {
-	static String s = "3people unFollowed me";
+	static String s = "3people unFollowed me   ";
 	public static void main(String[] args) {
 		String answer = "";
-		String[] in_arr = s.toLowerCase().split(" ");
+		String s2 = s.toLowerCase();
+		String[] in_arr = s2.split(" ");
 		
 		for(int i = 0 ; i < in_arr.length;i++) {
-			if(in_arr[i].length() >= 1) {
+			if(in_arr[i].length() >= 1){
+				
 				char[] tmp = in_arr[i].toCharArray();
 				tmp[0] = Character.toUpperCase(tmp[0]);
+				
 				in_arr[i] = new String(tmp);
 			}
 		}
 		answer = in_arr[0];
 		for(int i = 1 ; i < in_arr.length;i++) {
-			answer += " "+ in_arr[i]; 
+			answer += " " + in_arr[i];
+		}
+		if(s.length() != answer.length()) {
+			int cnt = s.length() - answer.length();
+			for(int i = 0 ; i < cnt;i++) {
+				answer += " ";
+			}
 		}
 		System.out.println(answer);
 	}
