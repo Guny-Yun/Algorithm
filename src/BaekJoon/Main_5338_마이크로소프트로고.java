c9d7453d9b165031eb936a8f81dc2e6fe5f670c6
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-08-22 03:21:30 +0900

    백준 5338 마이크로소프트 로고

---
+++ src/BaekJoon/Main_5338_마이크로소프트로고.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+public class Main_5338_마이크로소프트로고 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("       _.-;;-._\n");
+		sb.append("'-..-'|   ||   |\n");
+		sb.append("'-..-'|_.-;;-._|\n");
+		sb.append("'-..-'|   ||   |\n");
+		sb.append("'-..-'|_.-''-._|\n");
+		System.out.println(sb);
+	}
+
+}
