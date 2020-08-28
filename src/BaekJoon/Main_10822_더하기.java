ab729e4b0303a74643071e45137557ec119cf821
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-08-28 21:50:29 +0900

    백준 10822 더하기

---
+++ src/BaekJoon/Main_10822_더하기.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_10822_더하기 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(",");
+		int sum = 0;
+		
+		for(int i = 0 ; i < arr.length;i++) {
+			sum += Integer.parseInt(arr[i]);
+		}
+		System.out.println(sum);
+	}
+
+}
