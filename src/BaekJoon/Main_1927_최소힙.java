7522e62618453642b077f71b9472a107f353abf0
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-10-27 15:00:28 +0900

    백준 1927 최소 힙

---
+++ src/BaekJoon/Main_1927_최소힙.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main_1927_최소힙 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		
+		for(int i =0; i < N;i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			
+			if(tmp == 0) {
+				if(pq.isEmpty()) {
+					sb.append("0" + "\n");
+				}else {
+					sb.append(pq.poll() + "\n");
+				}
+			}else if(tmp > 0) {
+				pq.offer(tmp);
+			}
+		}
+		System.out.println(sb);
+	}
+}
