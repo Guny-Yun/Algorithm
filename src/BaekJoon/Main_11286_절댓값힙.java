13261b7e5438bf41dbc2499202ac155074f79ce6
Guny <58545186+Guny-Yun@users.noreply.github.com>
2020-10-29 00:35:41 +0900

    백준 11286 절댓값 힙

---
+++ src/BaekJoon/Main_11286_절댓값힙.java
@@ -0,0 +1,32 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main_11286_절댓값힙 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
+            int abs1 = Math.abs(o1);
+            int abs2 = Math.abs(o2);
+
+            if(abs1 == abs2) return o1 > o2 ? 1 : -1;
+            return abs1 - abs2;
+        });
+
+        for(int i = 0 ; i < N; i++){
+            int val = Integer.parseInt(br.readLine());
+            if(val == 0){
+                if(queue.isEmpty()) System.out.println("0");
+                else System.out.println(queue.poll());
+            }else{
+                queue.add(val);
+            }
+        }
+	}
+
+}
