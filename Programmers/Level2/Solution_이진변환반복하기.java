503b723177aae9d6fb6d98cb611796c8771dbeb6
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-03-21 22:24:28 +0900

    프로그래머스 lv.2 이진 변환 반복하기

---
+++ Programmers/Level2/Solution_이진변환반복하기.java
@@ -0,0 +1,24 @@
+package Level2;
+
+public class Solution_이진변환반복하기 {
+	static String s = "1111111";
+	public static void main(String[] args) {
+		int[] answer = {};
+		answer = new int[2];
+		
+		int cnt = 0;
+		int zero_cnt = 0;
+		
+		while(!s.equals("1")) {
+			int one_cnt = 0;
+			for(int i = 0 ; i < s.length();i++) {
+				if(s.charAt(i) == '1') one_cnt++;
+				else zero_cnt++;
+			}
+			s = Integer.toBinaryString(one_cnt);
+			cnt++;
+		}
+		answer[0] = cnt;
+		answer[1] = zero_cnt;
+	}
+}
