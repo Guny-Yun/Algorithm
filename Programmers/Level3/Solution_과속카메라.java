2f464d4cf512eb5fdc352c6465581d18627ca89d
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-05-07 09:40:43 +0900

    프로그래머스 lv.3 과속카메라

---
+++ Programmers/Level3/Solution_과속카메라.java
@@ -0,0 +1,31 @@
+package Level3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution_과속카메라 {
+
+	public static void main(String[] args) {
+		int[][] routes = {{},{},{},{}};
+		System.out.println(solution(routes));
+	}
+	public static int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2){
+                return o1[1] - o2[1];
+            }
+        });
+        
+        int camera = Integer.MIN_VALUE;
+        
+        for(int[] route : routes){
+            if(camera < route[0]){
+                camera = route[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
