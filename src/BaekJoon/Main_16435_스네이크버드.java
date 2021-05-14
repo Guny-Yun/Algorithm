0513efe8039e52bdf7578d0ce16b5353c980f209
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-05-14 13:02:16 +0900

    백준 16435 스네이크버드

---
+++ src/BaekJoon/Main_16435_스네이크버드.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_16435_스네이크버드 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		for(int i = 0 ; i < N;i++) {
+			if(arr[i] <= L)L++;
+			else break;
+		}
+		System.out.println(L);
+	}
+}
