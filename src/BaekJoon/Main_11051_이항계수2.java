86c8512baa57d4d81adfdc2416de7454066f6af6
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-07-06 23:30:55 +0900

    백준 11051 이항 계수2

---
+++ src/BaekJoon/Main_11051_이항계수2.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_11051_이항계수2 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N= Integer.parseInt(st.nextToken());
+		int K= Integer.parseInt(st.nextToken());
+		int[][] dp = new int[N+1][N+1];
+		
+		for(int i = 0 ; i < dp.length; i++) {
+			for(int j = 0; j <= i; j++) {
+				if(i == j || j == 0)dp[i][j] = 1;
+				else dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % 10007;
+			}
+		}
+		System.out.println(dp[N][K]);
+	}
+}
