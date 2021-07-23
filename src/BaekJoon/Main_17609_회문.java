f526732d3d5dd408636ea388b5985b395194b7b0
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-07-23 23:32:15 +0900

    백준 17609 회문 (오답수정)

--- src/BaekJoon/Main_17609_회문.java
+++ src/BaekJoon/Main_17609_회문.java
@@ -4,60 +4,51 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main_17609_회문 {
-	
+	static char[] arr;
+	static String s;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
-		String word = "";
-		int answer = 0;
-		
-		for(int i=0; i<T; i++) {
-			word = br.readLine();
-			answer = pal(word);
-			
-			System.out.println(answer);
-		}
-	}
-	public static int pal(String w) {
-		int res = 0;
-		int left = 0, right = w.length()-1;
-		
-		while(left <= right) {
-			if(w.charAt(left) == w.charAt(right)) {
-				left++;
-				right--;
-			}else {
-				int l = left;
-				int r = right;
-				l++;
-				while(l <= r) {
-					if(w.charAt(l) == w.charAt(r)) {
-						l++;
-						r--;
-					}else {
-						res++;
-						break;
-					}
-				}
-				l = left;
-				r = right;
-				r--;
-				
-				while(l <= r) {
-					if(w.charAt(l) == w.charAt(r)) {
-						l++;
-						r--;
-					}else {
-						res++;
-						break;
-					}
-				}
-				return res;
-			}
-			
-		}
-		return res;
-	}
 
+		for(int i=0; i<T; i++) {
+			s = br.readLine();
+			arr=s.toCharArray();
+			int left=0;
+			int right=s.length()-1;
+			if(check(left,right)) {
+				System.out.println(0);
+				continue;
+			}
+			if(checkS(left,right)) {
+				System.out.println(1);
+			}else {
+				System.out.println(2);
+			}
+		}
+	}
+	public static boolean check(int l, int r) {
+		while(l <= r) {
+			if(arr[l] != arr[r])return false;
+			
+			l++;
+			r--;
+		}
+		return true;
+	}
+	public static boolean checkS(int l, int r) {
+		while(l <= r) {
+			if(arr[l] != arr[r]) {
+				boolean a = check(l+1, r);
+				boolean b = check(l, r-1);
+				
+				if(!a && !b) {
+					return false;
+				}else 
+					return true;
+			}
+			l++;
+			r--;
+		}
+		return true;
+	}
 }
