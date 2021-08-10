af8c4b61dc88d0c4785de01770429eb0f1ffc6bc
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-08-11 01:07:31 +0900

    프로그래머스 Lv1 3진법 뒤집기

---
+++ Programmers/Level1/Solution_3진법뒤집기.java
@@ -0,0 +1,27 @@
+package Level1;
+
+import java.util.ArrayList;
+
+public class Solution_3진법뒤집기 {
+
+	public static void main(String[] args) {
+		System.out.println(solution(45));
+	}	
+	public static int solution(int n) {
+        int answer = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(true) {
+        	if(n < 3) {
+        		list.add(n);
+        		break;
+        	}
+        	list.add(n%3);
+        	n = n/3;
+        }
+        for(int i = 0; i < list.size();i++) {
+        	answer += (Math.pow(3, list.size()-i-1) * list.get(i));
+        }
+
+        return answer;
+    }
+}	
