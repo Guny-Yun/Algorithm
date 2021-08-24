0db01216734773b5e2f56a7611caa9e0ac82cf43
Guny <58545186+Guny-Yun@users.noreply.github.com>
2021-08-24 23:37:00 +0900

    백준 1021 회전하는 큐

---
+++ src/BOJ/Main_1021_회전하는큐.java
@@ -0,0 +1,42 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main_1021_회전하는큐 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		LinkedList<Integer> list = new LinkedList<>();
+		for(int i = 1; i <= N;i++) {
+			list.add(i);
+		}
+		int answer = 0;
+		int num = 0;
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < M;i++) {
+			num = Integer.parseInt(st.nextToken());
+			
+			while(num != list.get(0)) {
+				if(list.indexOf(num) <= list.size() / 2) {
+					int tmp = list.getFirst();
+					list.removeFirst();
+					list.addLast(tmp);
+				}else {
+					int tmp = list.getLast();
+					list.removeLast();
+					list.addFirst(tmp);
+				}
+				answer++;
+			}
+			list.removeFirst();
+		}
+		System.out.println(answer);
+	}
+}
